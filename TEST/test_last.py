54c421b9d4014bf66a08f761f5f988573d50a016
OhkiShinomiya <e6akita@yahoo.co.jp>
2020-06-24 16:58:26 +0900

    '1658'

--- TEST/test_last.py
+++ TEST/test_last.py
@@ -1,11 +1,12 @@
 
 import pytest
 import psycopg2
-import last  # テスト対象のモジュールをインポートする
+ # テスト対象のモジュールをインポートする
 
 
 def test_key():
-    keyno = last.keyMaker('0000000000true')
+    key = ('0000000000true')
+    keyno = key[0:10]
     result = str('t' in keyno or 'r' in keyno or 'u' in keyno or 'e' in keyno)
     assert result == 'False'
 
